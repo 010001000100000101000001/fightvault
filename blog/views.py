ecc82ba0a5145b26749d85db74e3ce7af57112ac
David Anderson <davidanderson@live.ie>
2024-04-29 21:41:02 +0000

    Created new directories and files for base template and homepage, Implemented a PostDetail view and added URL pattern in blog/urls.py. Created  and updated index.html template to use new post_detail view for post details. Adjusted base.html and index.html to improve styling and functionality. Fixed NoReverseMatch error and adjusted urlpatterns for fightvault/urls.py to include blog.urls. Removed redundant template

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,9 @@ from .models import Post
 # Class based views
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
