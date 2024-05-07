083bf5e960200f9c90c30fe380185a3d24ee6d7b
David Anderson <davidanderson@live.ie>
2024-05-07 13:29:03 +0000

    Set up about app and added a landscape image, applied styles for the contact page to the syle.css file

---
+++ contact/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import InquiryForm
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = InquiryForm(request.POST)
+        if form.is_valid():
+            return redirect('thank_you')
+    else:
+        form = InquiryForm()
+
+    return render(request, 'contact/contact.html', {'form': form})
