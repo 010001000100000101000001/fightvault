12fd15e3091b082819027598dbac1d162bdcd356
David Anderson <davidanderson@live.ie>
2024-05-27 17:55:07 +0000

    Add URL pattern for rate_post view to handle user ratings

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('custom-logout/', views.custom_logout, name='custom_logout'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('rate-post/<int:post_id>/', views.rate_post, name='rate_post'),
 ]
