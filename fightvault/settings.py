22540d7489c4d2c284a7905b6d93702bdf75ce20
David Anderson <davidanderson@live.ie>
2024-06-21 13:48:26 +0000

    Add ?secure=true to cloudinary url string to force https, issue highlighted from Lighthouse testing

--- fightvault/settings.py
+++ fightvault/settings.py
@@ -47,14 +47,6 @@ INSTALLED_APPS = [
 ]
 
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-    'SECURE': True,  # Enforce HTTPS for all Cloudinary URLs
-}
-
-
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 SITE_ID = 1
